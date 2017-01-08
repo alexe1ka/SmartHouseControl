e41b988c7f8e13ba70e130f0b6f248e6553e8791
alexe1ka <leshka_09@bk.ru>
2017-01-08 14:07:03 +0300

    Before DB and with working server timeout

---
+++ IotAlexe1ka/app/src/main/java/com/example/alexe1ka/iotalexe1ka/data/DbSchema.java
@@ -0,0 +1,10 @@
+package com.example.alexe1ka.iotalexe1ka.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by alexe1ka on 07.01.2017.
+ */
+
+public class DbSchema implements BaseColumns {
+}
